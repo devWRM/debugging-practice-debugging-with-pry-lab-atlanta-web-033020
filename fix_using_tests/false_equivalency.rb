cea55283dc0321552cecdcdf2b51e2c2c867e651
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-03-20 19:48:32 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -14,9 +14,12 @@ def selection(num)
   
   if num = 1
      "YUM YUM MUNCH MUNCH MUNCH"
+     
+     binding.pry
+     
   elsif num = 2
   
-  binding.pry
+  
   
      "HAM HAM HAM IN MY TUMMY"
   end
